6a90a66ce423fff6506e94a1a0464bec4f6cc200
hazardeurrr <thibault.drouot@gmail.com>
2021-11-30 23:11:31 +0100

    test1

--- src/services/oauth1.ts
+++ src/services/oauth1.ts
@@ -32,7 +32,7 @@ export const obtainOauthRequestToken = async ({
     consumerKey,
     consumerSecret
   });
-  const res = await fetch(`https://cors.bridged.cc/${apiUrl}`, {
+  const res = await fetch(`https://cors-serv.herokuapp.com/${apiUrl}`, {
     method,
     headers: {
       Authorization: `OAuth ${oauthSignature}`
@@ -65,7 +65,7 @@ export const obtainOauthAccessToken = async ({
     oauthToken,
     oauthVerifier
   });
-  const res = await fetch(`https://cors.bridged.cc/${apiUrl}`, {
+  const res = await fetch(`https://cors-serv.herokuapp.com/${apiUrl}`, {
     method,
     headers: {
       Authorization: `OAuth ${oauthSignature}`
